bc3c95b9aedd0ea746129e40ed0caab3a9d7448f
awhamil <andrew.hamilton@gmail.com>
2024-01-23 15:44:07 -0500

    autogenerate range of angles

--- makeMRZ.py
+++ makeMRZ.py
@@ -4,11 +4,28 @@ from PIL import ImageDraw
 import math
 import sys
 import random
+import os
+import pathlib
 
 MRZ_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789<"
 MRZ_MAX_CHARS = 44
 FONT_HEIGHT = 18 # pixels
 MRZ_CHAR_HEIGHT = 5 # height of MRZ in chars
+DEFAULT_OUTPUT_DIR = "output"
+DEFAULT_MAX_ANGLE = 5 # degrees
+
+show_boxes = '-b' in sys.argv
+write_classes_file = '-nc' not in sys.argv
+try:
+  dir_index = sys.argv.index("-d")
+  output_dir = sys.argv[dir_index + 1]
+except ValueError:
+  output_dir = DEFAULT_OUTPUT_DIR
+try:
+  angle_index = sys.argv.index("-a")
+  max_angle = int(sys.argv[angle_index + 1])
+except ValueError:
+  max_angle = DEFAULT_MAX_ANGLE
 
 def get_line1_offset(deg, cw, ch):
   rad = math.radians(deg)
@@ -53,9 +70,18 @@ def get_mrz_corner(deg, img_size, mrz_height):
   return coords
 
 def draw_line(deg, xf, yf, cw, ch, img):
+  chars = []
+  chars_info = []
   rad = math.radians(deg)
+  if write_classes_file:
+    classes_path = os.path.join(output_dir, "classes.txt")
+    with open(classes_path, "wt") as f:
+      for char in MRZ_CHARS:
+        f.write(f"{char}\n")
   for i in range(MRZ_MAX_CHARS):
-    c = MRZ_CHARS[random.randrange(len(MRZ_CHARS))]
+    c_idx = random.randrange(len(MRZ_CHARS))
+    c = MRZ_CHARS[c_idx]
+    chars.append(c)
     x = round(xf)
     y = round(yf)
     xt = cw * math.cos(rad)
@@ -67,28 +93,48 @@ def draw_line(deg, xf, yf, cw, ch, img):
     img2 = img2.rotate(deg, expand=1, resample=Image.BICUBIC)
     img.paste(img2, (x, y), img2)
     draw = ImageDraw.Draw(img)
-    draw.rectangle([x, y, x + img2.size[0] - 1, y + img2.size[1] - 1], outline="red")
+    x2 = x + img2.size[0] - 1
+    y2 = y + img2.size[1] - 1
+    if show_boxes:
+      draw.rectangle([x, y, x2, y2], outline="red")
+    x_pct = ((x2 + x) / 2) / img.size[0]
+    y_pct = ((y2 + y) / 2) / img.size[1]
+    w_pct = (x2 - x) / img.size[0]
+    h_pct = (y2 - y) / img.size[1]
+    chars_info.append(f"{c_idx} {x_pct} {y_pct} {w_pct} {h_pct}")
     xf = xf + xt
     yf = yf - yt
- 
-deg = int(sys.argv[1])
-bg = (250, 240, 230)
-ocrb = ImageFont.truetype('/home/awhamil/.local/share/fonts/OcrB2.ttf', FONT_HEIGHT)
-(_, _, cw, ch) = ocrb.getbbox("X")
-mrz_height = MRZ_CHAR_HEIGHT * ch
-img_size = (cw * (MRZ_MAX_CHARS + 2), mrz_height)
-img = Image.new(mode = "RGB", size = img_size, color = bg)
-img = img.rotate(deg, expand=1, resample=Image.BICUBIC)
-draw = ImageDraw.Draw(img)
-mrz_corner = get_mrz_corner(deg, img.size, mrz_height)
-line1_offset = get_line1_offset(deg, cw, ch)
-xf = mrz_corner[0] + line1_offset[0]
-yf = mrz_corner[1] + line1_offset[1]
-draw.ellipse([(xf - 2, yf - 2), (xf + 2, yf + 2)], fill="blue")
-draw_line(deg, xf, yf, cw, ch, img)
-line2_offset = get_line2_offset(deg, cw, ch)
-xf = mrz_corner[0] + line2_offset[0]
-yf = mrz_corner[1] + line2_offset[1]
-draw_line(deg, xf, yf, cw, ch, img)
- 
-img.show()
+  return {"line": ''.join(chars), "chars_info": chars_info}
+
+pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
+for d in range(max_angle + 1):
+  for sign in [-1, 1]:
+    deg = d * sign
+    bg = (250, 240, 230)
+    ocrb = ImageFont.truetype('/home/awhamil/.local/share/fonts/OcrB2.ttf', FONT_HEIGHT)
+    (_, _, cw, ch) = ocrb.getbbox("X")
+    mrz_height = MRZ_CHAR_HEIGHT * ch
+    img_size = (cw * (MRZ_MAX_CHARS + 2), mrz_height)
+    img = Image.new(mode = "RGB", size = img_size, color = bg)
+    img = img.rotate(deg, expand=1, resample=Image.BICUBIC, fillcolor="white")
+    draw = ImageDraw.Draw(img)
+    mrz_corner = get_mrz_corner(deg, img.size, mrz_height)
+    line1_offset = get_line1_offset(deg, cw, ch)
+    xf = mrz_corner[0] + line1_offset[0]
+    yf = mrz_corner[1] + line1_offset[1]
+    #draw.ellipse([(xf - 2, yf - 2), (xf + 2, yf + 2)], fill="blue")
+    line1 = draw_line(deg, xf, yf, cw, ch, img)
+    line2_offset = get_line2_offset(deg, cw, ch)
+    xf = mrz_corner[0] + line2_offset[0]
+    yf = mrz_corner[1] + line2_offset[1]
+    line2 = draw_line(deg, xf, yf, cw, ch, img)
+
+    # write output
+    text_path = os.path.join(output_dir, f"{line1['line']}_{deg}.txt")
+    with open(text_path, "wt") as f:
+      for char_info in line1["chars_info"]:
+        f.write(f"{char_info}\n")
+      for char_info in line2["chars_info"]:
+        f.write(f"{char_info}\n")
+    img_path = os.path.join(output_dir, f"{line1['line']}_{deg}.jpg")
+    img.save(img_path)
